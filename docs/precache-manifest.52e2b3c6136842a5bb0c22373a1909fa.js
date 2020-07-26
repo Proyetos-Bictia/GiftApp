648486af3a4cd0bfe23715b61eb17a0d0160e10d
Camilo Davila <camilo7davila@gmail.com>
2020-07-26 12:37:08 -0500

    docs add

---
+++ docs/precache-manifest.52e2b3c6136842a5bb0c22373a1909fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5d427a1e58e44acff31227b7445ae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "801e0af371c94a40a84c",
+    "url": "/static/css/main.b721d031.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801e0af371c94a40a84c",
+    "url": "/static/js/main.3cb4828c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
